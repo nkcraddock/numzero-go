aa251e406a54d59f8a9029bee44b060e228baf57
Nathan Craddock <nkcraddock@gmail.com>
2015-04-29 22:02:48 -0400

    extracted a server testing harness so we can integration test the web api without having to stand it up listening on a port

--- server/companies_test.go
+++ server/companies_test.go
@@ -1,14 +1,24 @@
 package server_test
 
 import (
+	"net/http"
+
+	"github.com/nkcraddock/gooby/server"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("CompanyResource integration tests", func() {
+	cfg := &server.ServerConfig{}
+	server := NewServerHarness(cfg)
+
 	Context("GET /companies", func() {
 		It("retrieves a list of companies", func() {
-			Ω("").Should(Equal(""))
+			companies := []string{}
+			res := server.GET("/companies", &companies)
+			Ω(companies).ShouldNot(BeEmpty())
+			Ω(companies).Should(HaveLen(2))
+			Ω(res.Code).Should(Equal(http.StatusOK))
 		})
 	})
 })
--- server/suite_server_test.go
+++ server/suite_server_test.go
@@ -1,8 +1,14 @@
 package server_test
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/emicklei/go-restful"
+	"github.com/nkcraddock/gooby/server"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -10,4 +16,30 @@ import (
 func TestServer(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Server Suite")
+}
+
+type ServerHarness struct {
+	container *restful.Container
+}
+
+func NewServerHarness(cfg *server.ServerConfig) *ServerHarness {
+	c := server.BuildContainer(cfg)
+	return &ServerHarness{container: c}
+}
+
+func (s *ServerHarness) GET(uri string, data interface{}) (res *httptest.ResponseRecorder) {
+	req, _ := http.NewRequest("GET", uri, nil)
+	req.Header.Set("Accept", "application/json")
+	res = httptest.NewRecorder()
+
+	s.container.ServeHTTP(res, req)
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	json.Unmarshal(body, &data)
+
+	return
 }
