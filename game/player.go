9154e8738b69565c3989188a80a65e5be4b6147e
Nathan Craddock <nkcraddock@gmail.com>
2015-05-23 21:35:50 -0400

    added player image urls. fixed a font issue.

--- game/player.go
+++ game/player.go
@@ -3,11 +3,13 @@ package game
 type Player struct {
 	Name  string `json:"name"`
 	Score int    `json:"score"`
+	Image string `json:"image"`
 }
 
 func NewPlayer(name string) *Player {
 	return &Player{
 		Name:  name,
+		Image: "http://placehold.it/160x160",
 		Score: 0,
 	}
 }
